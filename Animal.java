8b522c5dc4f8cac951fb95c7a246707debb5f43c
shruti2193 <shrutidixit2193@gmail.com>
2024-09-12 12:15:21 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -2,8 +2,9 @@ package myanimals;
 
 public class Animal {
     public void animalsound () {
-        // there is not a static keyword available here
+        // there is not a static keyword available here// create a static named numoanimals
         System.out.println("\n A SOUND FROM THE ANIMAL CLASS ");
-
-    }
+static int numofanimals = 0;
+public Animal();
+numofanimals++;}
 }
