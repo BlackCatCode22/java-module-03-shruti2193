3edd42cf219291b5a4c00f0dffe07ee3aad4ca9a
shruti2193 <shrutidixit2193@gmail.com>
2024-09-17 12:10:07 -0700

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -1,7 +1,36 @@
-package myanimals;
+public class Dog {
+    // create two fields for our dog objects.
+    private String getname;
+    private int age;
+    public void setName(String name) {
+        name = name;
 
-public class Dog extends Animal{
-    public void animalsound() {
-        System.out.println("\n A SOUND FROM THE dog CLASS used by a dog object - bark");
     }
+    // create a constructor for all the ibjects that will be created with the new " keywords.
+    public Dog() {
+        System.out.println("/n a dog class is created! this message is from the constructor \n ");
+
+    }
+
+        public Dog(String myName) {
+        this.name = myName;
+    }
+    public Dog(int theAge){
+        this.age= theAge;
+    }
+    // create constructor that accepts two arguments - a string for the name, and an int for age.
+    public Dog(String somename,int someAge){
+        // assign name and age to oue new object
+        this.age = someAge;
+        this.name = somename;
+    }
+
 }
+ public int getAge () {
+    return this.age;
+
+
+ }
+ public void setAge(int age){
+    age=age;
+ }
---
+++ MainClassPractice.java
@@ -0,0 +1,30 @@
+//sf 9/17/24
+public class MainClassPractice {
+
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("\n welcome to my OOP Class Practice Program!\n");
+         Dog myDog = new Dog();
+         Dog anotherDog= new Dog();
+
+         // create a dog with a String name
+        int myName;
+        Dog aDog;
+        aDog = new Dog(myName;:"Spot");
+        System.out.println("\n The new dog's name is :" + aDog.getname);
+        // create a fourth dog with int age.
+        int theAge;
+        Dog fourthdog = new Dog(theAge:33);
+        System.out.println("\n The new dog's age is :" + aDog.age);
+        int someName;
+        Dog fifthdog = new Dog(someName: "my new dogs name is + " + fifthdog.name + "and age");
+        // give birthday
+        fifthdog.setAge(fifthdog.getAge()+1);
+        System.out.println("MY DOG,"+ fifthdog.getName() + ", had a birthday and age is now: " + fifthdog.getName);
+
+
+
+        }
+    }
