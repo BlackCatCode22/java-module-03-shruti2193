460c88268ecc861f431d930400f1129ea6a8b00d
shruti2193 <shrutidixit2193@gmail.com>
2024-09-19 12:17:07 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,10 +1,43 @@
-package myanimals;
-
 public class Animal {
-    public void animalsound () {
-        // there is not a static keyword available here// create a static named numoanimals
-        System.out.println("\n A SOUND FROM THE ANIMAL CLASS ");
-static int numofanimals = 0;
-public Animal();
-numofanimals++;}
+    // creae a static int that keep track of the number of the animals created.
+    static int numofanimals = 0;
+    // ceate a constructor for our new animal objects
+    public Animal () {
+        numofanimals++;
+
+    }
+    //Create a few fields
+    // Sex will be male or female
+    private String sex = "";
+    private int age = 0;
+    // weight will be in pounds
+    private int weight = 0;
+    // Create getters and setters
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
+
---
+++ Lion.java
@@ -0,0 +1,3 @@
+public class Lion extends Animal{
+    // WE ARE IN THE LION CLASS
+}
